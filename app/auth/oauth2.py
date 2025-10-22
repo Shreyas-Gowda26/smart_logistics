26a3c0316bd4186363e354bbff7bbb3fdb6a2c5e
Shreyas Gowda <shreyasg2526@gmail.com>
2025-10-22 12:59:45 +0530

    Done with the auth

---
+++ app/auth/oauth2.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException,Depends,status
+from fastapi.security import OAuth2PasswordBearer
+from app.auth.token import verify_token
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    user_id = verify_token(token, credentials_exception)
+    return user_id
